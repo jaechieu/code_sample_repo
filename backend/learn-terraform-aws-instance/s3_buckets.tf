95bc88966e8661e679ac4e2882608d318ff1bbcb
jaechieu <jeechieu.ta321@gmail.com>
2024-07-06 18:27:57 -0700

    adding resource blocks for s3 buckets

--- backend/learn-terraform-aws-instance/s3_buckets.tf
+++ backend/learn-terraform-aws-instance/s3_buckets.tf
@@ -1,11 +1,19 @@
 variable "custom_model_bucket" {
   description = ""
   type        = string
-  default     = "CustomModelBucket"
+  default     = "chatbot-custom-model-bucket"
+}
+
+resource "aws_s3_bucket" "custom_model_bucket" {
+  bucket = var.custom_model_bucket
 }
 
 variable "finetuning_data_bucket" {
   description = ""
   type        = string
-  default     = "FinetuningDataBucket"
+  default     = "chatbot-finetuning-data-bucket"
+}
+
+resource "aws_s3_bucket" "finetuning_data_bucket" {
+  bucket = var.finetuning_data_bucket
 }
